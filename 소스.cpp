0dc4ddf23e672df9640ef3c9e7c569de025b9408
배지현 <miel0806@naver.com>
2022-07-05 23:49:53 +0900

    #2908

--- 소스.cpp
+++ 소스.cpp
@@ -5,24 +5,36 @@ using namespace std;
 
 int main()
 {
-	string s{};
-	getline(cin, s);
+	string s1{};
+	string s2{};
 
-	int count{};
+	cin >> s1;
+	cin >> s2;
 
-	for (int i{}; i < s.size(); ++i) {
-		if (isspace(s[i])) {
-			count++;
+	string S1{};
+	string S2{};
+
+	for (int i = s1.size() - 1; i >= 0; --i) {
+		S1 += s1[i];
+	}
+
+	for (int i = s2.size() - 1; i >= 0; --i) {
+		S2 += s2[i];
+	}
+
+	for (int i{}; i < S1.size(); ++i) {
+		if (S1[i] > S2[i]) {
+			cout << S1 << endl;
+			break;
+		}
+
+		else if (S1[i] < S2[i]) {
+			cout << S2 << endl;
+			break;
+		}
+
+		else {
+			continue;
 		}
 	}
-
-	if (isspace(s[0])) {
-		count--;
-	}
-
-	if (isspace(s[s.size() - 1])) {
-		count--;
-	}
-
-	cout << count + 1 << endl;
 }

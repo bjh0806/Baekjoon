4cf77ef19cb157a8996c3d64cd08fc7096ae4501
배지현 <miel0806@naver.com>
2022-07-13 23:10:26 +0900

    #1193

--- 소스.cpp
+++ 소스.cpp
@@ -9,16 +9,46 @@ int main()
 	cin >> num;
 
 	long long count = 1;
+	long long n{};
 
 	while (1) {
-		if ((3 * pow(count, 2) - 3 * count + 1) >= num) {
-			break;
+		if ((pow(count, 2) + count) / 2 < num) {
+			count++;
 		}
 
 		else {
-			count++;
+			break;
 		}
 	}
 
-	cout << count << endl;
+	long long n1{};
+	long long n2{};
+
+	if ((pow(count, 2) + count) / 2 != num) {
+		n = (pow(count, 2) + count) / 2 - num;
+
+		if (count % 2 == 0) {
+			n1 = count - n;
+			n2 = 1 + n;
+		}
+
+		else {
+			n1 = 1 + n;
+			n2 = count - n;
+		}
+	}
+
+	else {
+		if (count % 2 == 0) {
+			n1 = count;
+			n2 = 1;
+		}
+
+		else {
+			n1 = 1;
+			n2 = count;
+		}
+	}
+
+	cout << n1 << "/" << n2 << endl;
 }

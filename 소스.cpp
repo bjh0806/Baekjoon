ebfaca4f6db028caa60b03b15edde63856e9279a
배지현 <miel0806@naver.com>
2022-07-14 23:57:36 +0900

    #2869 시간 초과

--- 소스.cpp
+++ 소스.cpp
@@ -1,54 +1,34 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	long long num{};
-	cin >> num;
+	long long A{};
+	long long B{};
+	long long V{};
 
-	long long count = 1;
-	long long n{};
+	cin >> A >> B >> V;
+
+	long long num = 1;
+	long long result{};
 
 	while (1) {
-		if ((pow(count, 2) + count) / 2 < num) {
-			count++;
-		}
-
-		else {
+		if (result + A - B >= V) {
 			break;
 		}
-	}
-
-	long long n1{};
-	long long n2{};
-
-	if ((pow(count, 2) + count) / 2 != num) {
-		n = (pow(count, 2) + count) / 2 - num;
-
-		if (count % 2 == 0) {
-			n1 = count - n;
-			n2 = 1 + n;
-		}
 
 		else {
-			n1 = 1 + n;
-			n2 = count - n;
+			if (result + A >= V) {
+				break;
+			}
+
+			else {
+				result += (A - B);
+				num++;
+			}
 		}
 	}
 
-	else {
-		if (count % 2 == 0) {
-			n1 = count;
-			n2 = 1;
-		}
-
-		else {
-			n1 = 1;
-			n2 = count;
-		}
-	}
-
-	cout << n1 << "/" << n2 << endl;
+	cout << num << endl;
 }

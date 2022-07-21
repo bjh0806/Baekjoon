51a092480387847f44c8519aade3d0005b57f3ad
배지현 <miel0806@naver.com>
2022-07-21 22:43:39 +0900

    #2581

--- 소스.cpp
+++ 소스.cpp
@@ -4,40 +4,52 @@ using namespace std;
 
 int main()
 {
+	int M{};
 	int N{};
-	cin >> N;
+	cin >> M >> N;
 
-	int num[100]{};
-
-	for (int i{}; i < N; ++i) {
-		cin >> num[i];
-	}
-
+	int min = 10001;
+	int sum{};
 	int count{};
 	int n = 3;
 
-	for (int i{}; i < N; ++i) {
+	for (int i = M; i <= N; ++i) {
 		n = 3;
 
-		if (num[i] != 1 && num[i] == 2 || num[i] == 3 || num[i] == 5 || num[i] == 7) {
+		if (i != 1 && i == 2 || i == 3 || i == 5 || i == 7) {
+			sum += i;
 			count++;
-			continue;
+
+			if (i <= min) {
+				min = i;
+			}
 		}
 
-		else if (num[i] % 2 != 0) {
-			while (n <= num[i] / 2) {
-				if (num[i] % n == 0) {
+		else if (i % 2 != 0) {
+			while (n <= i / 2) {
+				if (i % n == 0) {
 					break;
 				}
 
 				n += 2;
 
-				if (n > num[i] / 2) {
+				if (n > i / 2) {
+					sum += i;
 					count++;
+
+					if (i <= min) {
+						min = i;
+					}
 				}
 			}
 		}
 	}
 
-	cout << count << endl;
+	if (count == 0) {
+		cout << -1 << endl;
+	}
+
+	else {
+		cout << sum << endl << min << endl;
+	}
 }

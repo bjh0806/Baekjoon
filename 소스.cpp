3240ab2064d0e8b5e6a803e31f8fc0dbdf2674a3
배지현 <miel0806@naver.com>
2022-07-22 23:54:50 +0900

    #11653 일부 진행

--- 소스.cpp
+++ 소스.cpp
@@ -4,25 +4,34 @@ using namespace std;
 
 int main()
 {
-	int M{};
-	int N{};
-	cin >> M >> N;
+	long long N{};
+	cin >> N;
 
-	int min = 10001;
-	int sum{};
+	long long n[5000001]{};
 	int count{};
-	int n = 3;
 
-	for (int i = M; i <= N; ++i) {
-		n = 3;
+	while (N != 1) {
+		int num = 2;
+		int number = 3;
+
+		if (N % num == 0) {
+			n[count] = num;
+			N /= num;
+			count++;
+		}
+
+		while (num <= N / 2) {
+			if (num % number == 0) {
+				num += 2;
+			}
+		}
+	}
+
+	for (int i{}; i <= N; ++i) {
+		num = 3;
 
 		if (i != 1 && i == 2 || i == 3 || i == 5 || i == 7) {
-			sum += i;
 			count++;
-
-			if (i <= min) {
-				min = i;
-			}
 		}
 
 		else if (i % 2 != 0) {
@@ -36,10 +45,6 @@ int main()
 				if (n > i / 2) {
 					sum += i;
 					count++;
-
-					if (i <= min) {
-						min = i;
-					}
 				}
 			}
 		}

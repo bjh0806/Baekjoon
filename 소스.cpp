6b01dea2e08e24b60089400779bb5bf54e80bc5c
배지현 <miel0806@naver.com>
2022-07-25 23:54:45 +0900

    #1929 시간 초과 개선

--- 소스.cpp
+++ 소스.cpp
@@ -8,16 +8,21 @@ int main()
 	int N{};
 	cin >> M >> N;
 
+	if (M <= 2) {
+		cout << 2 << endl;
+		M = 3;
+	}
+
 	int n = 3;
 
-	for (int i = M; i <= N; ++i) {
+	for (int i = M; i <= N; i += 2) {
 		n = 3;
 
-		if (i != 1 && i == 2 || i == 3 || i == 5 || i == 7) {
+		if (i == 3 || i == 5 || i == 7) {
 			cout << i << endl;
 		}
 
-		else if (i % 2 != 0) {
+		else {
 			while (n <= i / 2) {
 				if (i % n == 0) {
 					break;

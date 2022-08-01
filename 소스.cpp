a9a025d9acbeabdc25cac3ada7efbe8cd709b2cd
배지현 <miel0806@naver.com>
2022-08-01 23:05:25 +0900

    #9020 일부 진행
    첫 번째 소수 값이 최소인 경우로 해결, 두 소수의 차가 최소가 되도록 변경할 필요가 있음

--- 소스.cpp
+++ 소스.cpp
@@ -18,46 +18,20 @@ int main()
 
 	for (int i{}; i < T; ++i) {
 		for (int j = 2; j < N[i]; ++j) {
-			int n = 2;
+			if (result[1] == 0) {
+				int n = 2;
 
-			if (j != 1 && j == 2 || j == 3 || j == 5 || j == 7) {
-				result[0] = j;
+				if (j != 1 && j == 2 || j == 3 || j == 5 || j == 7) {
+					result[0] = j;
 
-				if (N[i] - j == 2 || N[i] - j == 3 || N[i] - j == 5 || N[i] - j == 7) {
-					result[1] = N[i] - j;
-					break;
-				}
-				
-				else {
-					while (n <= sqrt(N[i] - j)) {
-						if (N[i] - j % n == 0) {
-							break;
-						}
-
-						n++;
-
-						if (n > sqrt(N[i] - j)) {
-							result[1] = N[i] - j;
-							break;
-						}
-					}
-				}
-			}
-
-			else if (j % 2 != 0) {
-				while (n <= sqrt(j)) {
-					if (j % n == 0) {
+					if (N[i] - j == 2 || N[i] - j == 3 || N[i] - j == 5 || N[i] - j == 7) {
+						result[1] = N[i] - j;
 						break;
 					}
 
-					n++;
-
-					if (n > sqrt(j)) {
-						result[0] = j;
-						n = 2;
-
+					else {
 						while (n <= sqrt(N[i] - j)) {
-							if (N[i] - j % n == 0) {
+							if ((N[i] - j) % n == 0) {
 								break;
 							}
 
@@ -69,9 +43,40 @@ int main()
 						}
 					}
 				}
+
+				else if (j % 2 != 0) {
+					while (n <= sqrt(j)) {
+						if (j % n == 0) {
+							break;
+						}
+
+						n++;
+
+						if (n > sqrt(j)) {
+							result[0] = j;
+							n = 2;
+
+							while (n <= sqrt(N[i] - j)) {
+								if (N[i] - j % n == 0) {
+									break;
+								}
+
+								n++;
+
+								if (n > sqrt(N[i] - j)) {
+									result[1] = N[i] - j;
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 
 		cout << result[0] << " " << result[1] << "\n";
+
+		for (int j{}; j < 2; ++j) {
+			result[j] = 0;
+		}
 	}
 }

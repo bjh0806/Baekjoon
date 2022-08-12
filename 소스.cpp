159fc6d3bba84caf3c449eb8cdfd306a155c84fc
배지현 <miel0806@naver.com>
2022-08-12 23:43:29 +0900

    #2447 일부 진행
    N이 3일 경우 해결, 그 외 추가 필요

--- 소스.cpp
+++ 소스.cpp
@@ -2,53 +2,36 @@
 
 using namespace std;
 
-void Recursion(int n, int count)
+void Recursion(int n)
 {
-	for (int i{}; i < count; ++i) {
-		cout << "____";
+	for (int i{}; i < n; ++i) {
+		cout << "*";
 	}
 
-	cout << "\"재귀함수가 뭔가요?\"" << endl;
+	cout << endl;
 
-	for (int i{}; i < count; ++i) {
-		cout << "____";
+	for (int i{}; i < n / 3; ++i) {
+		cout << "*";
 	}
 
-	if (n != 0) {
-		cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." << endl;
-
-		for (int i{}; i < count; ++i) {
-			cout << "____";
-		}
-
-		cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." << endl;
-
-		for (int i{}; i < count; ++i) {
-			cout << "____";
-		}
-
-		cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" << endl;
+	for (int i{}; i < n / 3; ++i) {
+		cout << " ";
 	}
 
-	else {
-		cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << endl;
+	for (int i{}; i < n / 3; ++i) {
+		cout << "*";
 	}
 
-	if (n - 1 >= 0) {
-		Recursion(n - 1, count + 1);
-	}
-}
+	cout << endl;
 
-void recursion(int n, int count)
-{
-	for (int i{}; i < count; ++i) {
-		cout << "____";
+	for (int i{}; i < n; ++i) {
+		cout << "*";
 	}
 
-	cout << "라고 답변하였지." << endl;
+	cout << endl;
 
-	if (n - 1 >= 0) {
-		recursion(n - 1, count - 1);
+	if (n - 3 != 0) {
+		Recursion(n - 3);
 	}
 }
 
@@ -57,7 +40,5 @@ int main()
 	int N{};
 	cin >> N;
 
-	cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." << endl;
-	Recursion(N, 0);
-	recursion(N, N);
+	Recursion(N);
 }

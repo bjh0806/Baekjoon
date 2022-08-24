827b1ea3c0b6c2f883107013957ac0ef1409136b
배지현 <miel0806@naver.com>
2022-08-24 23:44:27 +0900

    #2447 일부 수정
    가운데에 큰 빈칸 생기도록 수정

--- 소스.cpp
+++ 소스.cpp
@@ -16,8 +16,8 @@ void recursion(int x, int y, int n, int N)
 void Recursion(int x, int y, int n, int N)
 {
 	if (n == 3) {
-		for (int i = x; i < x + 3; ++i) {
-			for (int j = y; j < y + 3; ++j) {
+		for (int i = x; i < x + N / 3; ++i) {
+			for (int j = y; j < y + N / 3; ++j) {
 				if (i % 3 == j % 3 && i % 3 == 1) {
 					star[i][j] = " ";
 				}
@@ -40,14 +40,14 @@ void Recursion(int x, int y, int n, int N)
 	}
 
 	else {
-		for (int i{}; i <= N / 3; ++i) {
-			for (int j{}; j <= N / 3; ++j) {
+		for (int i{}; i <= 3; ++i) {
+			for (int j{}; j <= 3; ++j) {
 				if (i % 3 == j % 3 && i % 3 == 1) {
-					recursion(i * 3, j * 3, n / 3, N);
+					recursion(i * N / 3, j * N / 3, n / 3, N);
 				}
 
 				else {
-					Recursion(i * 3, j * 3, n / 3, N);
+					Recursion(i * N / 3, j * N / 3, n / 3, N);
 				}
 			}
 
